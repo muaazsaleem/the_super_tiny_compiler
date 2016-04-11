bd2e5fed7b39626652eff139e137e40ea0b2b1a1
Muaaz Saleem <muaazs@plumgrid.com>
2016-04-11 06:40:31 -0700

    Added a parser for the super_tiny_compiler
    Signed-off-by: Muaaz Saleem <muaazs@plumgrid.com>

--- lib/the_super_tiny_compiler.ex
+++ lib/the_super_tiny_compiler.ex
@@ -42,12 +42,12 @@ defmodule TheSuperTinyCompiler do
     ast
   end
 
-  def parser([%Token{ type: "number", value: number_val } | rest] = tokens, ast) do
+  def parser([%Token{ type: "number", value: number_val } | rest], ast) do
     number_node = %Token{ type: "NumberLiteral", value: number_val }
     parser(rest, %Ast{type: "Program", body: [number_node | ast.body]})
   end
 
-  def parser([%Token{ type: "paren", value: "(" } | rest] = tokens, ast)  do
+  def parser([%Token{ type: "paren", value: "(" } | rest], ast)  do
     [call_name | params] = rest
     {call_ast, rest } = parser(params)
     call_node = %Token{ type: "CallExpression", name: call_name.value, params: Enum.reverse(call_ast.body)}
